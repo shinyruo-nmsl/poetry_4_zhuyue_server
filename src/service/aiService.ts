479623ecd98910877651e7af53d5a3020df1153c
jim <jiminhu@trip.com>
2024-06-24 20:40:05 +0800

    替换为azure_openai

--- src/service/aiService.ts
+++ src/service/aiService.ts
@@ -1,32 +1,61 @@
-import OpenAI from "openai";
+import {
+  OpenAIClient,
+  AzureKeyCredential,
+  EventStream,
+  ChatCompletions,
+} from "@azure/openai";
 import { CustomError } from "./errorService";
 
 export class AIServer {
-  private static openai = new OpenAI({
-    apiKey: process.env.OPENAI_SECRET_KEY,
-  });
+  private static openai = new OpenAIClient(
+    process.env.AZURE_OPENAI_ENDPOINT,
+    new AzureKeyCredential(process.env.AZURE_OPENAI_SECRET_KEY)
+  );
 
   static async createStream(message: string) {
     try {
-      const stream = await this.openai.chat.completions.create({
-        model: "gpt-4",
-        messages: [{ role: "user", content: message }],
-        stream: true,
-      });
+      const messages = [
+        {
+          role: "system",
+          content: "You are a helpful assistant. You will talk like a pirate.",
+        },
+        { role: "user", content: "Can you help me?" },
+        {
+          role: "assistant",
+          content: "Arrrr! Of course, me hearty! What can I do for ye?",
+        },
+        { role: "user", content: "What's the best way to train a parrot?" },
+      ];
+      const stream = await this.openai.streamChatCompletions(
+        "jimgpt35",
+        messages,
+        { maxTokens: 128 }
+      );
       return new AIStream(stream);
     } catch (error) {
-      throw new CustomError("gpt连接异常", "other", error);
+      // throw new CustomError("gpt连接异常", "other", error);
+      throw error;
     }
+  }
+
+  static async test() {
+    const { choices } = await this.openai.getCompletions(
+      "jimgpt35", // assumes a matching model deployment or model name
+      ["如何在nodejs里面使用azure openai"]
+    );
+
+    console.log("start");
+    for (const choice of choices) {
+      console.log(choice.text);
+    }
+    console.log("end");
   }
 }
 
-type OpenAIStream = Exclude<
-  Awaited<ReturnType<OpenAI["chat"]["completions"]["create"]>>,
-  OpenAI.Chat.Completions.ChatCompletion
->;
+AIServer.test();
 
 class AIStream implements AsyncIterable<string> {
-  constructor(private stream: OpenAIStream) {}
+  constructor(private stream: EventStream<ChatCompletions>) {}
 
   async *[Symbol.asyncIterator]() {
     for await (const chunk of this.stream) {
@@ -34,3 +63,13 @@ class AIStream implements AsyncIterable<string> {
     }
   }
 }
+
+// async function main() {
+//   const ai = await AIServer.createStream("hello");
+
+//   for await (const content of ai) {
+//     console.log(content);
+//   }
+// }
+
+// main();
