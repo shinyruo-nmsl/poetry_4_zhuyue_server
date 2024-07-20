697644d6a1875b19e9c5a0324c510943304d0a98
胡济民 <hujimin@hujimindeMacBook-Pro.local>
2024-07-20 23:28:55 +0800

    提高payload大小

--- src/service/middlewareService.ts
+++ src/service/middlewareService.ts
@@ -25,7 +25,7 @@ export default function handleMiddleware(
 ) {
   app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
   app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-  app.use(express.json());
+  app.use(express.json({ limit: "50mb" }));
 
   const { trace } = MiddleWare.middleWares;
   app.use(trace());
